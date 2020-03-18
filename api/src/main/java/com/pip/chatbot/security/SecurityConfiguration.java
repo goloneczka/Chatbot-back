7c634b73b9c918096842cb45883d9fffd9fe2f24
goloneczka <48791689+goloneczka@users.noreply.github.com>
2020-03-18 15:46:09 +0100

    cookies

--- api/src/main/java/com/pip/chatbot/security/SecurityConfiguration.java
+++ api/src/main/java/com/pip/chatbot/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -33,12 +34,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+
+        http.sessionManagement().
+                sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/" + ADMIN.toLowerCase() + "/login").permitAll()
                 .antMatchers("/" + ADMIN.toLowerCase() + "/**").hasRole("ADMIN")
                 .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
                 .and().httpBasic();
     }
 }
