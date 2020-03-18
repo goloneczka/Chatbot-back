6f1c024a87b73a90063cbaf155b2c43f2ea18a08
goloneczka <48791689+goloneczka@users.noreply.github.com>
2020-03-18 10:31:01 +0100

    code review, encode password

--- api/src/main/java/com/pip/chatbot/security/SecurityConfiguration.java
+++ api/src/main/java/com/pip/chatbot/security/SecurityConfiguration.java
@@ -1,52 +1,42 @@
 package com.pip.chatbot.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @EnableWebSecurity
+@PropertySource("application.yml")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private static final String ADMIN = "ADMIN";
 
+    @Value("${admin.password}")
+    private String password;
+
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put("bcrypt", new BCryptPasswordEncoder());
-        encoders.put("scrypt", new SCryptPasswordEncoder());
-
-        return new DelegatingPasswordEncoder(
-                "bcrypt", encoders);
+        return new BCryptPasswordEncoder();
     }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("admin").password(getPasswordEncoder().encode("admin")).roles(ADMIN)
-                .and()
-                .withUser("admin1").password(getPasswordEncoder().encode("admin1")).roles(ADMIN);
+                .withUser("admin").password(password).roles(ADMIN);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/admin/login").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/" + ADMIN.toLowerCase() + "/login").permitAll()
+                .antMatchers("/" + ADMIN.toLowerCase() + "/**").hasRole("ADMIN")
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic();
