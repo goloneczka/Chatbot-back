8bea9542666df30c236bd7821f33301c5a4130cb
Michacy45 <44591148+Michacy45@users.noreply.github.com>
2020-03-31 13:17:48 +0200

    maven profile activating allTests

---
+++ api/src/test/java/com/pip/chatbot/test/SampleTest.java
@@ -0,0 +1,12 @@
+package com.pip.chatbot.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTest {
+    @Test
+    void sampleTest(){
+        System.out.println("Api Test");
+    }
+}
