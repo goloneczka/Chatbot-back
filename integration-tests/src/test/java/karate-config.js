908b784ab72182b87886989ddf6bc9bcbe34a457
Adam <adam.krzanowski1998@gmail.com>
2020-04-15 10:27:20 +0200

    Zmiana portu dla testów

--- integration-tests/src/test/java/karate-config.js
+++ integration-tests/src/test/java/karate-config.js
@@ -3,6 +3,6 @@ function fn() {
         user: 'admin',
         password: 'Admin123*pip-2020',
         incorrectPassword: 'Admin123*pip+2020',
-        baseUrl: "http://localhost:9090"
+        baseUrl: "http://localhost:8081"
     }
 }
