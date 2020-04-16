16502ef2839c2b39ffec79e4e87256567b96e81e
goloneczka <48791689+goloneczka@users.noreply.github.com>
2020-04-16 17:59:43 +0200

    refactor dev database

---
+++ database/src/main/resources/db/migration/V4__jokes_joke_add_mark_column.sql
@@ -0,0 +1,11 @@
+SET search_path TO jokes;
+
+ALTER TABLE JOKE
+    add column is_confirmed boolean DEFAULT true not null;
+
+CREATE TABLE IF NOT EXISTS mark
+(
+    id                    SERIAL PRIMARY KEY,
+    joke_id               INTEGER not null references joke(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    mark                  DECIMAL not null
+);
