11b33f717928e3b06b198e080fcd706e7e8156bd
Michacy45 <44591148+Michacy45@users.noreply.github.com>
2020-04-22 14:26:51 +0200

    added food schema

--- database/src/main/java/com/pip/chatbot/jooq/DefaultCatalog.java
+++ database/src/main/java/com/pip/chatbot/jooq/DefaultCatalog.java
@@ -4,6 +4,7 @@
 package com.pip.chatbot.jooq;
 
 
+import com.pip.chatbot.jooq.food.Food;
 import com.pip.chatbot.jooq.jokes.Jokes;
 import com.pip.chatbot.jooq.weather.Weather;
 
@@ -20,12 +21,17 @@ import org.jooq.impl.CatalogImpl;
 @SuppressWarnings({ "all", "unchecked", "rawtypes" })
 public class DefaultCatalog extends CatalogImpl {
 
-    private static final long serialVersionUID = -1725149449;
+    private static final long serialVersionUID = -771396625;
 
     /**
      * The reference instance of <code>DEFAULT_CATALOG</code>
      */
     public static final DefaultCatalog DEFAULT_CATALOG = new DefaultCatalog();
+
+    /**
+     * The schema <code>food</code>.
+     */
+    public final Food FOOD = Food.FOOD;
 
     /**
      * The schema <code>jokes</code>.
@@ -47,6 +53,7 @@ public class DefaultCatalog extends CatalogImpl {
     @Override
     public final List<Schema> getSchemas() {
         return Arrays.<Schema>asList(
+            Food.FOOD,
             Jokes.JOKES,
             Weather.WEATHER);
     }
