e6a9101070a0bdbcbe32bbb262d8fdb795bc7d43
piotr3290 <piotr3290@wp.pl>
2020-05-11 03:24:22 +0200

    Usunięcie menu

--- integration/src/main/java/com/pip/chatbot/integration/food/ScheduledRestaurants.java
+++ integration/src/main/java/com/pip/chatbot/integration/food/ScheduledRestaurants.java
@@ -2,7 +2,6 @@ package com.pip.chatbot.integration.food;
 
 import com.pip.chatbot.model.food.City;
 import com.pip.chatbot.repository.food.FoodCitiesRepository;
-import com.pip.chatbot.repository.food.MenuRepository;
 import com.pip.chatbot.repository.food.RestaurantRepository;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,14 +12,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledRestaurants {
     private final FoodCitiesRepository foodCitiesRepository;
-    private final MenuRepository menuRepository;
     private final RestaurantRepository restaurantRepository;
     private final ZomatoApi zomatoApi;
 
 
-    public ScheduledRestaurants(FoodCitiesRepository foodCitiesRepository, MenuRepository menuRepository, RestaurantRepository restaurantRepository, ZomatoApi zomatoApi) {
+    public ScheduledRestaurants(FoodCitiesRepository foodCitiesRepository, RestaurantRepository restaurantRepository, ZomatoApi zomatoApi) {
         this.foodCitiesRepository = foodCitiesRepository;
-        this.menuRepository = menuRepository;
         this.restaurantRepository = restaurantRepository;
         this.zomatoApi = zomatoApi;
     }
@@ -31,13 +28,6 @@ public class ScheduledRestaurants {
             restaurantRepository.createRestaurants(zomatoApi.getRestaurantsData(city));
         }
     }
-
-//    @Scheduled(fixedDelayString = "${application.zomato.restaurantDelay}")
-//    public void saveMenusData() throws Exception {
-//        for(Restaurant restaurant : restaurantRepository.getAllRestaurants()){
-//            zomatoApi.getMenusData(restaurant);
-//        }
-//    }
 
 
 }
--- integration/src/main/java/com/pip/chatbot/integration/food/ZomatoApi.java
+++ integration/src/main/java/com/pip/chatbot/integration/food/ZomatoApi.java
@@ -3,7 +3,6 @@ package com.pip.chatbot.integration.food;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pip.chatbot.model.food.City;
-import com.pip.chatbot.model.food.Menu;
 import com.pip.chatbot.model.food.Restaurant;
 import org.springframework.stereotype.Component;
 
@@ -11,42 +10,49 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class ZomatoApi {
     private final ZomatoConfig zomatoConfig;
     private final ObjectMapper objectMapper;
+    private final int restaurantsPerPage;
+    private final int maxPageNumber;
 
 
-    public ZomatoApi(ZomatoConfig zomatoConfig, ObjectMapper objectMapper){
+    public ZomatoApi(ZomatoConfig zomatoConfig, ObjectMapper objectMapper) {
         this.zomatoConfig = zomatoConfig;
         this.objectMapper = objectMapper;
+        this.restaurantsPerPage = 20;
+        this.maxPageNumber = 5;
     }
 
     public List<Restaurant> getRestaurantsData(City city) throws Exception {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://developers.zomato.com/api/v2.1/search?entity_id=" + city.getId() + "&entity_type=city"))
-                .header("user-key", zomatoConfig.getKey())
-                .GET()
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        List<Restaurant> allRestaurants = new ArrayList<>();
+        int pageNumber = 0;
+        int pagesAmount = maxPageNumber;
 
-        String restaurantDataJSON = objectMapper.readTree(response.body()).get("restaurants").findValues("restaurant").toString();
+        do {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://developers.zomato.com/api/v2.1/search?entity_id=" + city.getId() + "&entity_type=city&sort=rating&start=" + pageNumber * this.restaurantsPerPage))
+                    .header("user-key", zomatoConfig.getKey())
+                    .GET()
+                    .build();
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        return objectMapper.readValue(restaurantDataJSON, new TypeReference<List<Restaurant>>() {});
+            String restaurantDataJSON = objectMapper.readTree(response.body()).get("restaurants").findValues("restaurant").toString();
+
+            allRestaurants.addAll(objectMapper.readValue(restaurantDataJSON, new TypeReference<List<Restaurant>>() {
+            }));
+
+            pagesAmount = (int) Math.min(Math.ceil(objectMapper.readTree(response.body()).get("results_found").floatValue()), this.maxPageNumber);
+
+        } while (pageNumber++ < pagesAmount);
+
+
+        return allRestaurants;
     }
 
-    public Menu getMenusData(Restaurant restaurant) throws Exception {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://developers.zomato.com/api/v2.1/dailymenu?res_id=" + restaurant.getId()))
-                .header("user-key", zomatoConfig.getKey())
-                .GET()
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.toString());
-        return null;
-    }
 }
