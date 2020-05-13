afccdf720c2d5481e6e9e487a231e78ffde82058
Adam <adam.krzanowski1998@gmail.com>
2020-05-13 09:21:43 +0200

    BugFix

--- api/src/main/java/com/pip/chatbot/service/food/FoodService.java
+++ api/src/main/java/com/pip/chatbot/service/food/FoodService.java
@@ -1,13 +1,17 @@
 package com.pip.chatbot.service.food;
 
+import com.pip.chatbot.exception.ChatbotException;
 import com.pip.chatbot.exception.ChatbotExceptionBuilder;
 import com.pip.chatbot.exception.messages.FoodErrorMessages;
+import com.pip.chatbot.exception.messages.JokesErrorMessages;
+import com.pip.chatbot.exception.messages.MarksErrorMessages;
 import com.pip.chatbot.model.food.*;
 import com.pip.chatbot.repository.food.FoodRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -25,7 +29,12 @@ public class FoodService {
     public Restaurant getRandomRestaurantForCuisine(Integer cityId, String cuisine) {
         return foodRepository.getRandomRestaurantForCuisine(cityId, cuisine)
                 .map(restaurant -> {
-                    foodRepository.getAvgRestaurantMark(restaurant.getId()).ifPresent((average) -> restaurant.setAverageUsersRating((restaurant.getAverageUsersRating() + average) / 2));
+                    try {
+                        Double average = foodRepository.getAvgRestaurantMark(restaurant.getId()).get();
+                        restaurant.setAverageUsersRating((average + restaurant.getAverageUsersRating()) / 2);
+                    } catch (NullPointerException e) {
+                        e.getStackTrace();
+                    }
                 return restaurant;
                 })
                 .orElseThrow(() -> new ChatbotExceptionBuilder().addError(FoodErrorMessages.RESTAURANT_NOT_FOUND).build());
