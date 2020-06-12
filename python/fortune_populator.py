af2bfab5a2d47d386d77b573c3d566224d463fa9
Michacy45 <44591148+Michacy45@users.noreply.github.com>
2020-06-12 02:49:07 +0200

    Names refactor, added 'try/catch', changed logs

--- python/fortune_populator.py
+++ python/fortune_populator.py
@@ -11,35 +11,40 @@ USER = "app"
 PASSWORD = "Ao4eiT2w"
 DATABASE = "appdb"
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
-def populate_db(df, symbol, cursor, is_historical):
+def populate_table(df, symbol, cursor, is_historical):
     data = df['Close'].to_numpy().flatten()
     indexes = df.index
     extras.execute_values(cursor, "INSERT into stock(symbol, value, date, is_historical) values %s",
                           [(symbol, data[i], indexes[i], is_historical) for i in range(len(indexes))])
 
 
-def scheduled_stocks():
+def populate_db():
     with pg.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, database=DATABASE) as conn:
         cursor = conn.cursor()
         cursor.execute("SET search_path to fortune")
         cursor.execute("SELECT symbol FROM symbol")
         symbols = cursor.fetchall()
-        logging.debug("Symbols from db: ", symbols)
+        logging.info(f"Symbols from db: {symbols}")
         for symbol in symbols:
-            logging.debug("Using symbol: ", symbol)
+            logging.info(f"Using symbol: {symbol}")
 
-            stocks = web.DataReader(symbol[0], data_source='yahoo', start=date.today(), end=date.today())
-            logging.debug("Stocks number: ", len(stocks))
-            populate_db(stocks, symbol, cursor, True)
+            stocks = web.DataReader(symbol[0], data_source='yahoo', start="2020-06-12", end="2020-06-12")
+            logging.info(f"Stocks number: {len(stocks)}")
+            populate_table(stocks, symbol, cursor, True)
 
             forecasts = predictions.learn_and_predict(symbol[0], "2000-01-01", date.today())
-            logging.debug("Forecasts number: ", len(forecasts))
-            populate_db(forecasts, symbol, cursor, False)
+            logging.info(f"Forecasts number: {len(forecasts)}")
+            populate_table(forecasts, symbol, cursor, False)
 
             conn.commit()
 
 
-scheduled_stocks()
+try:
+    populate_db()
+except KeyError as e:
+    logging.error("Stocks not found")
+except Exception as e:
+    logging.error(repr(e))
